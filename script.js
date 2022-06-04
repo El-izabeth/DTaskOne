970aee035f5b06c50320b4660589a9ee7043e0e3
El-izabeth <95127403+El-izabeth@users.noreply.github.com>
2022-06-04 23:00:18 +0530

    2ndCommit

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 
+ /*Logic:
+ one button blinks.
+ User clicks it.
+ Move to next round.
+ same button blinks, follwed by next random button blinks.
+ user clicks both in whatever sequence.
+ next round.
+ 1st button blinks. Next, 2nd button blinks. Next, 3rd button blinks.
+ etc..until last round or until user clicks wrong button*/
 var points = 0;
 var arrin=[];
 var arr=[];
@@ -18,42 +27,20 @@ var getRand = (function() {
 function randArr(){
     
     arr = Array.from(document.getElementsByClassName('tile'));
-    //let count = 0;
-    //let i = 0;
     
-    //let interval1 = setInterval(function(){
         document.getElementById("pts").innerHTML=points;
         idarr=[];
         x = getRand();
         
         arrin.push(x);
         f = blink(arrin,arr); 
-        /*if(f==false){
-            gameOver();
-            //clearInterval(interval1);
-            
-        } */
-                                                                                /*blinks as many times as 
-                                                                                there are elements 
-                                                                                in arrel*/
-        /*let id = arr[x].id;
-        document.getElementById(id).style.backgroundColor = '#e223dc';          //change clr
-        setTimeout(function(){                                                  //back to og clr
-            document.getElementById(id).style.backgroundColor = '#9e177a';
-        },500);*/
-        
-        
-        //i++;
+       
         if(arrin.length==16){
-            
+            document.getElementById("W/L").innerHTML="YOU WIN!!";
             gameOver();
-            //clearInterval(interval1);
+            
             
         }
-        
-        
-        
-    //},2000);
 }
 
 function tileClick(id){
@@ -61,27 +48,23 @@ function tileClick(id){
     if(idarr.length==arrin.length){
         checkClick();
         randArr();
-    }
-    //alert(arrin.length);
-    
+    }    
 }
 function checkClick(){
-    //c=0;
-    for(i = 0; i < idarr.length;i++){
+    for(i = 0; i < arrin.length;i++){
         if(idarr.includes(arr[arrin[i]])){
-            //c++;
             incrementPoints();
             continue;
         }
         else{
+            document.getElementById("W/L").innerHTML="YOU LOSE!";
             gameOver();
+            
             
         }
     }
-    //document.getElementById(pts).innerHTML="points";
 }
 function incrementPoints(){
-    //alert("IP");
     points+=10;
 }
 function blink(arrin,arr){
@@ -95,30 +78,20 @@ function blink(arrin,arr){
         },500);
         
     }
-    
-    
 }
 
- /*Logic:
- one button blinks.
- User clicks it.
- Move to next round.
- same button blinks, follwed by next random button blinks.
- user clicks both in whatever sequence.
- next round.
- 1st button blinks. Next, 2nd button blinks. Next, 3rd button blinks.
- etc..until last round or until user clicks wrong button*/
- function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-  }
   function gameOver(){
-      alert("GAMEOVER\nPOINTS:"+points);
-      arrin=[];
-      return;
-
+    alert("GAMEOVER\nPOINTS:"+points);
+    points = 0;
+    arrin=[];
+    exit();
+    //arrin=[];
+    
       
+  }
+  function exit(){
+      while(1>0){
+
+      }
+     
   }
